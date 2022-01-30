49c23455126c23a540f21b0e2d831b1bc99fb169
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-01-30 23:10:24 +0800

    Remove headshot for now

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { Row, Col, Image } from "react-bootstrap";
 import { markdownToHtml } from "../lib/markdown";
 
 interface AboutPageProps {
@@ -8,34 +6,13 @@ interface AboutPageProps {
 }
 
 const About = ({ bio }: AboutPageProps): React.ReactElement => {
-  const router = useRouter();
-
   return (
     <>
       <Head>
         <title>About: Joshua Cerdenia Music</title>
       </Head>
       <div className="about-container">
-        <Row xs={1} xl={2}>
-          <Col xl={5}>
-            <div className="mb-4">
-              <Image
-                className="headshot shadowed"
-                src="images/headshot.jpg"
-                onClick={() => router.push("/images/headshot.jpg")}
-                alt="headshot"
-                rounded
-                fluid
-              />
-              <figcaption className="small text-muted">
-                Photo by Rashidah De Vore
-              </figcaption>
-            </div>
-          </Col>
-          <Col xl={7}>
-            <section dangerouslySetInnerHTML={{ __html: bio }} />
-          </Col>
-        </Row>
+        <section dangerouslySetInnerHTML={{ __html: bio }} />
       </div>
     </>
   );
