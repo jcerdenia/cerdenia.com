637ea5cccf377d6fee930dfda91a6d73f15eed30
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-01-30 23:12:14 +0800

    Update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+import Head from "next/head";
 import { Container } from "react-bootstrap";
 import NavBar from "../components/NavBar";
 
 const MyApp = ({ Component, pageProps }): React.ReactElement => {
   return (
     <>
+      <Head>
+        <link rel="icon" href="music-note.svg" />
+      </Head>
       <NavBar />
       <Container>
         <Component {...pageProps} />
