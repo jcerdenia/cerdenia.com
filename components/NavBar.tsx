6675452e89a3f4a3d3c47c7490db238a38a49ace
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-02-01 16:31:43 +0800

    Add line break to brand

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,8 +8,8 @@ export default function NavBar(): React.ReactElement {
     <Navbar className="navbar" variant="light" expand="md">
       <Container>
         <Navbar.Brand className="brand" href="/">
-          Joshua Cerdenia{" "}
-          <span className="mx-1 sub-brand text-muted">Composer</span>
+          Joshua Cerdenia
+          <div className="sub-brand">Composer</div>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
