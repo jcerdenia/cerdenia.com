af25415553032a381e6f307f6cc6fb91acf802ed
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-02-15 20:42:54 +0800

    Add meta tags to PageWrapper

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -2,13 +2,23 @@ import Head from "next/head";
 
 interface PageWrapperProps {
   title: string;
-  children: any;
+  description?: string;
+  children?: any;
 }
 
-const PageWrapper = ({ title, children }: PageWrapperProps) => {
+const PageWrapper = ({ title, description, children }: PageWrapperProps) => {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:image"
+          content="https://music.cerdenia.com/images/headshot-2.jpeg"
+        />
+        <link rel="icon" href="music-note.svg" />
         <title>{title}</title>
       </Head>
       {children}
