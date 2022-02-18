237988d12f0b4758a04ce323b13fd5f1642a0168
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-02-18 21:38:34 +0800

    Fix news item style

--- pages/news.tsx
+++ pages/news.tsx
@@ -12,11 +12,11 @@ const News = (): React.ReactElement => {
             {group.items.map((item: NewsItem, idx: number) => (
               <Row key={idx} className="news-item my-3">
                 <Col className="d-flex align-items-center" md={12} lg={4}>
-                  <div className="text-muted small news-item-meta">
+                  <div className="small news-item-meta">
                     <span>
                       <strong>{item.source}</strong>
                     </span>
-                    <span>{item.date}</span>
+                    <span className="text-muted">{item.date}</span>
                   </div>
                 </Col>
                 <Col className="d-flex align-items-center" md={12} lg={8}>
