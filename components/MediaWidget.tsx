3f8172ab094fbd182a69f1bb888a29dd00984871
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-02-22 23:30:35 +0800

    Darken other media widgets when hovering over one widget

--- components/MediaWidget.tsx
+++ components/MediaWidget.tsx
@@ -1,15 +1,21 @@
 interface MediaWidgetProps {
+  id?: string;
   className?: string;
   src: string;
   width?: number | string;
   height?: number | string;
+  onMouseEnter?: VoidFunction;
+  onMouseLeave?: VoidFunction;
 }
 
 const MediaWidget = ({
+  id,
   className,
   src,
   width = "100%",
   height = 300,
+  onMouseEnter,
+  onMouseLeave,
 }: MediaWidgetProps): React.ReactElement => {
   let type: "youtube" | "soundcloud" | undefined;
 
@@ -31,6 +37,7 @@ const MediaWidget = ({
     src = src.replace(/http(s?):\/\//i, "");
     return (
       <iframe
+        id={id}
         className={className}
         width={width}
         height={height}
@@ -38,6 +45,8 @@ const MediaWidget = ({
         frameBorder="no"
         allow="autoplay"
         src={`https://w.soundcloud.com/player/?url=https%3A//${src}&color=%23484440&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true`}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
       />
     );
   }
@@ -46,6 +55,7 @@ const MediaWidget = ({
     src = src.replace(/watch\?v=/i, "embed/");
     return (
       <iframe
+        id={id}
         className={className}
         width={width}
         height={height}
@@ -54,6 +64,8 @@ const MediaWidget = ({
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
       />
     );
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import PageWrapper from "../components/PageWrapper";
 import MediaWidget from "../components/MediaWidget";
 import media from "../data/showcase";
 
 const HomePage = (): React.ReactElement => {
+  const [hoveredWidgetId, setHoveredWidgetId] = useState(null);
+
+  useEffect(() => {
+    for (let i = 0; i < media.length; i++) {
+      const widgetId = `widget-${i}`;
+      const widgetEl = document.getElementById(widgetId);
+      widgetEl.style.transition = "300ms";
+
+      if (hoveredWidgetId && widgetId !== hoveredWidgetId) {
+        widgetEl.style.filter = "brightness(60%)";
+      } else {
+        widgetEl.style.filter = "brightness(100%)";
+      }
+    }
+  }, [hoveredWidgetId]);
+
   return (
     <PageWrapper exact title="Joshua Cerdenia, Composer">
-      <Row xs={1} lg={2} xl={3} className="g-1">
+      <Row xs={1} lg={2} xl={3} className="g-2">
         {media.map((src: string, idx: number) => (
-          <Col key={idx} className="showcase">
-            <MediaWidget src={src} />
+          <Col style={{ margin: 0 }} key={idx}>
+            <MediaWidget
+              className="showcase"
+              id={`widget-${idx}`}
+              src={src}
+              onMouseEnter={() => setHoveredWidgetId(`widget-${idx}`)}
+              onMouseLeave={() => setHoveredWidgetId(null)}
+            />
           </Col>
         ))}
       </Row>
