55f66b01e72560102e9f136822c39692605dabef
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-05-05 14:33:11 +0800

    Add linked labels to showcase items

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import PageLayout from "../components/PageLayout";
 import MediaWidget from "../components/MediaWidget";
-import media from "../data/showcase";
+import media, { MediaItem } from "../data/showcase";
 
 const HomePage = (): JSX.Element => {
   const [hoveredWidgetId, setHoveredWidgetId] = useState<number | null>(null);
@@ -14,18 +14,27 @@ const HomePage = (): JSX.Element => {
         : "brightness(50%)",
   });
 
+  const decideShowcaseLinkClass = (widgetId: number): string => {
+    return hoveredWidgetId !== widgetId ? "link-muted" : "showcase-link";
+  };
+
   return (
     <PageLayout exact title="Joshua Cerdenia, Composer">
-      <Row xs={1} lg={2} xl={3} className="g-1">
-        {media.map((src: string, idx: number) => (
+      <Row xs={1} lg={2} xl={3} className="g-2">
+        {media.map((item: MediaItem, idx: number) => (
           <Col className="showcase-container" key={idx}>
             <MediaWidget
               className="showcase"
-              src={src}
+              src={item.src}
               onMouseEnter={() => setHoveredWidgetId(idx)}
               onMouseLeave={() => setHoveredWidgetId(null)}
               style={decideWidgetStyle(idx)}
             />
+            <p className="small">
+              <a className={decideShowcaseLinkClass(idx)} href={item.path}>
+                {item.title}
+              </a>
+            </p>
           </Col>
         ))}
       </Row>
