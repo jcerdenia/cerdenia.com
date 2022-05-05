dde30c8a4df0f63fdf91fe7baa8f35f179bd6305
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-05-05 14:11:55 +0800

    Refactor MediaWidget

--- components/MediaWidget.tsx
+++ components/MediaWidget.tsx
@@ -17,60 +17,50 @@ const MediaWidget = ({
   onMouseEnter,
   onMouseLeave,
 }: MediaWidgetProps): JSX.Element => {
-  let type: "youtube" | "soundcloud" | undefined;
-
   switch (true) {
     case src.includes("youtube.com"): {
-      type = "youtube";
-      break;
+      src = src.replace(/watch\?v=/i, "embed/");
+
+      return (
+        <iframe
+          className={className}
+          width={width}
+          height={height}
+          style={style}
+          src={src}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        />
+      );
     }
+
     case src.includes("soundcloud.com"): {
-      type = "soundcloud";
-      break;
+      src = src.replace(/http(s?):\/\//i, "");
+
+      return (
+        <iframe
+          className={className}
+          width={width}
+          height={height}
+          style={style}
+          src={`https://w.soundcloud.com/player/?url=https%3A//${src}&color=%23484440&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true`}
+          scrolling="no"
+          frameBorder="no"
+          allow="autoplay"
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        />
+      );
     }
+
     default: {
-      type = undefined;
+      return <text className="text-danger">Cannot render {src}</text>;
     }
   }
-
-  if (type === "soundcloud") {
-    src = src.replace(/http(s?):\/\//i, "");
-    return (
-      <iframe
-        className={className}
-        width={width}
-        height={height}
-        style={style}
-        src={`https://w.soundcloud.com/player/?url=https%3A//${src}&color=%23484440&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true`}
-        scrolling="no"
-        frameBorder="no"
-        allow="autoplay"
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      />
-    );
-  }
-
-  if (type === "youtube") {
-    src = src.replace(/watch\?v=/i, "embed/");
-    return (
-      <iframe
-        className={className}
-        width={width}
-        height={height}
-        style={style}
-        src={src}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      />
-    );
-  }
-
-  return <text className="text-danger">Cannot render {src}</text>;
 };
 
 export default MediaWidget;
