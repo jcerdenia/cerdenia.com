daa6db9db76e07f507bff994d5e16d95b4eed701
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-05-09 22:53:20 +0800

    Update site meta description

--- data/meta.ts
+++ data/meta.ts
@@ -6,7 +6,7 @@ interface Meta {
 const meta: Meta = {
   title: "Joshua Cerdenia, Composer",
   description:
-    "The online home of the music of Joshua Cerdenia. List of works with audio samples and program notes, biography and photographs, news and upcoming events.",
+    "The online home of the music of Joshua Cerdenia. List of works with audio samples, program notes, and perusal scores; biography and photographs; news items and upcoming events.",
 };
 
 export default meta;
