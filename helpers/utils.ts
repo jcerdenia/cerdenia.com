db5f7b1bcad69e94f79adcc0aeb1feca327635e7
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-05-20 11:50:56 +0800

    Add dynamic date formatting

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,4 +1,74 @@
+// Get text up to first period and without HTML tags.
 export const excerpt = (text: string): string => {
-  // Get text up to first period and without HTML tags.
   return text.slice(0, text.indexOf(". ") + 1).replace(/(<([^>]+)>)/gi, "");
+};
+
+// Function to return a custom comparator for sorting objects by property.
+// Use with sort(), e.g., sort(compareBy("prop"))
+export const compareBy = (prop: string): ((a: any, b: any) => 1 | -1 | 0) => {
+  return (objA: any, objB: any) => {
+    const [a, b] = [objA, objB].map((obj) => {
+      return typeof obj[prop] === "string"
+        ? obj[prop].toLowerCase().replace(/\W/g, "")
+        : obj[prop];
+    });
+
+    return a < b ? -1 : a > b ? 1 : 0;
+  };
+};
+
+// Can receive raw date either as a single date, e.g., "2022-04-15"
+// Or as a range, e.g. "2022-04-15,2022-04-18"
+export const formatDate = (dateString: string, options: any = {}): string => {
+  const rawDates: string[] = dateString.split(",");
+
+  const dates: string[] = rawDates.map((rawDate) => {
+    const [year, month, day] = rawDate
+      .split("-")
+      .map((val: any): number => val * 1);
+
+    const date = new Date();
+    date.setFullYear(year);
+    date.setMonth(month - 1);
+    date.setDate(day || 1);
+
+    return date.toLocaleString("en-GB", {
+      day: day ? "numeric" : undefined,
+      month: "short",
+      year: "numeric",
+      ...options,
+    });
+  });
+
+  if (dates.length > 1) {
+    const days = dates.map((d) => d.split(" ")[0]);
+    const months = dates.map((m) => m.split(" ")[1]);
+    const years = dates.map((y) => y.split(" ")[2]);
+
+    const isSameMonth = months[0] === months[1];
+    const isSameYear = years[0] === years[1];
+
+    switch (true) {
+      case isSameYear && isSameMonth: {
+        const year = years[0];
+        const month = months[0];
+        const range = days.join("-");
+
+        return `${range} ${month} ${year}`;
+      }
+
+      case isSameYear && !isSameMonth: {
+        const year = years[0];
+        const rangeStart = `${days[0]} ${months[0]}`;
+        const rangeEnd = `${days[1]} ${months[1]}`;
+
+        return `${rangeStart}-${rangeEnd} ${year}`;
+      }
+
+      default:
+        return dates.join("-");
+    }
+  }
+
+  return dates[0];
 };
--- pages/news.tsx
+++ pages/news.tsx
@@ -3,6 +3,7 @@ import HtmlWrapper from "../components/HtmlWrapper";
 import PageLayout from "../components/PageLayout";
 import { parseMarkdownInline } from "../helpers/markdown";
 import { NewsItem, NewsItemGroup } from "../data/interfaces";
+import { compareBy, formatDate } from "../helpers/utils";
 import announcements from "../data/news/announcements.json";
 import articles from "../data/news/articles.json";
 import events from "../data/news/events.json";
@@ -20,30 +21,27 @@ const NewsPage = ({ news }: NewsPageProps): JSX.Element => {
             <h5>{group.title}</h5>
             {group.items.map((item: NewsItem, idx: number) => (
               <Row key={idx} className="news-item my-3">
-                <Col className="d-flex" lg={12} xl={4}>
-                  <div className="small news-item-line news-item-meta">
+                <Col lg={12} xl={4}>
+                  <div className="small text-height-1 news-item-meta">
                     <span className="text-bold">{item.source}</span>
                     <span className="text-muted text-right news-item-date">
-                      {item.date}
+                      {formatDate(item.date)}
                     </span>
                   </div>
                 </Col>
 
                 <Col lg={12} xl={8}>
-                  <a className="news-item-line" href={item.url}>
+                  <a className="text-height-1" href={item.url}>
                     {item.title}
                   </a>
                   {item.language && ` (${item.language})`}
                   {item.blurb && (
-                    <HtmlWrapper
-                      className="small news-item-line"
-                      parent="aside"
-                    >
+                    <HtmlWrapper className="text-height-1" parent="aside">
                       {item.blurb}
                     </HtmlWrapper>
                   )}
                   {item.location && (
-                    <aside className="small text-muted">{item.location}</aside>
+                    <aside className="text-muted">{item.location}</aside>
                   )}
                 </Col>
               </Row>
@@ -56,8 +54,9 @@ const NewsPage = ({ news }: NewsPageProps): JSX.Element => {
 };
 
 export const getStaticProps = async (): Promise<{ props: NewsPageProps }> => {
-  const isRecent = (event: NewsItem) => {
-    return event.date.split(" ").reverse()[0] >= "2021";
+  const isRecent = (item: NewsItem) => {
+    // Return only items dated 2021 and onward.
+    return item.date.split(" ").reverse()[0] >= "2021";
   };
 
   const news: NewsItemGroup[] = [
@@ -65,6 +64,7 @@ export const getStaticProps = async (): Promise<{ props: NewsPageProps }> => {
       title: "Recent & Upcoming Events",
       items: events
         .filter(isRecent)
+        .sort(compareBy("date"))
         .reverse()
         .map((item: NewsItem) => {
           item.blurb = item.blurb ? parseMarkdownInline(item.blurb) : null;
@@ -75,6 +75,7 @@ export const getStaticProps = async (): Promise<{ props: NewsPageProps }> => {
       title: "Recent Announcements",
       items: announcements
         .filter(isRecent)
+        .sort(compareBy("date"))
         .reverse()
         .map((item: NewsItem) => {
           delete item.blurb;
@@ -83,7 +84,7 @@ export const getStaticProps = async (): Promise<{ props: NewsPageProps }> => {
     },
     {
       title: "Articles, Reviews & Interviews",
-      items: articles.reverse(),
+      items: articles.sort(compareBy("date")).reverse(),
     },
   ];
 
