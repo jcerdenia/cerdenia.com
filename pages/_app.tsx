e77636421c68fa03ac41b2ac5cb6c9c03b9db2fc
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-08-04 08:49:16 +0800

    Move google analytics to _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,48 +1,10 @@
-import "../styles/globals.css";
-import Script from "next/script";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import AppStateProvider from "../components/AppStateProvider";
 import type { AppProps } from "next/app";
-import * as gtag from "../lib/gtag";
+import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <AppStateProvider>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-
       <Component {...pageProps} />
     </AppStateProvider>
   );
