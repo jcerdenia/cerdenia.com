53d4684c766389805a561a5165043e3299c70c9a
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-08-06 21:44:09 +0800

    Add more works link to showcase

--- components/Showcase.tsx
+++ components/Showcase.tsx
@@ -1,14 +1,21 @@
+import Link from "next/link";
 import { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import MediaWidget from "../components/MediaWidget";
+import LinkedIcon from "./LinkedIcon";
 import media, { MediaItem } from "../data/showcase";
 
 interface ShowcaseProps {
   columns?: number;
   limit?: number;
+  noMoreWorksLink?: boolean;
 }
 
-const Showcase = ({ columns = 3, limit = 6 }: ShowcaseProps): JSX.Element => {
+const Showcase = ({
+  columns = 3,
+  limit = 6,
+  noMoreWorksLink = false,
+}: ShowcaseProps): JSX.Element => {
   const [hoveredWidgetId, setHoveredWidgetId] = useState<number | null>(null);
 
   const getWidgetStyle = (widgetId: number): {} => ({
@@ -23,28 +30,40 @@ const Showcase = ({ columns = 3, limit = 6 }: ShowcaseProps): JSX.Element => {
   };
 
   return (
-    <Row xs={1} lg={columns} className="g-2 mb-4">
-      {media.slice(0, limit).map((item: MediaItem, idx: number) => {
-        return (
-          <Col key={idx}>
-            <MediaWidget
-              className="showcase"
-              src={item.src}
-              onMouseEnter={() => setHoveredWidgetId(idx)}
-              onMouseLeave={() => setHoveredWidgetId(null)}
-              style={getWidgetStyle(idx)}
-            />
+    <>
+      <Row xs={1} lg={columns} className="g-2 mb-4">
+        {media.slice(0, limit).map((item: MediaItem, idx: number) => {
+          return (
+            <Col key={idx}>
+              <MediaWidget
+                className="showcase"
+                src={item.src}
+                onMouseEnter={() => setHoveredWidgetId(idx)}
+                onMouseLeave={() => setHoveredWidgetId(null)}
+                style={getWidgetStyle(idx)}
+              />
 
-            <aside className="small">
-              <a className={getShowcaseLinkClass(idx)} href={item.path}>
-                {item.title}
-              </a>{" "}
-              <span className="text-muted">({item.artist})</span>
-            </aside>
-          </Col>
-        );
-      })}
-    </Row>
+              <aside className="small">
+                <Link href={item.path} passHref>
+                  <a className={getShowcaseLinkClass(idx)}>{item.title}</a>
+                </Link>{" "}
+                <span className="text-muted">({item.artist})</span>
+              </aside>
+            </Col>
+          );
+        })}
+      </Row>
+
+      {!noMoreWorksLink ? (
+        <LinkedIcon
+          className="link-muted"
+          iconId="bi:arrow-return-right"
+          href="/works"
+        >
+          More Works
+        </LinkedIcon>
+      ) : null}
+    </>
   );
 };
 
