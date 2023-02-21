6bbb91e07b89d644491968bc92b4b10d040497f1
Joshua Cerdenia <joshcerdenia@gmail.com>
2023-02-21 22:29:36 +0800

    refactor: arrange global tags into components

---
+++ components/tags/BootstrapCDN.tsx
@@ -0,0 +1,12 @@
+const BootstrapCDN = (): JSX.Element => {
+  return (
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+      crossOrigin="anonymous"
+    />
+  );
+};
+
+export default BootstrapCDN;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,37 +1,15 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-import { GA_TRACKING_ID } from "../lib/gtag";
+import BootstrapCDN from "../components/tags/BootstrapCDN";
+import GoogleAnalytics from "../components/tags/GoogleAnalytics";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-
-          {/* Bootstrap CDN */}
-          <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
-            crossOrigin="anonymous"
-          />
+          <GoogleAnalytics />
+          <BootstrapCDN />
 
           <link rel="icon" href="music-note.svg" />
         </Head>
